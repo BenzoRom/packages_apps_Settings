8004e474249ce4ae0bc0bdeba4a45eb1b19c676c
George G <kreach3r@users.noreply.github.com>
2016-07-04 12:07:11 +0300

    OMS: Apps: show/hide Substratum overlays [2/2]
    Default behavior is "hidden" substratum overlays,
    as they pollute the screen, and you can't notice the
    actual apps.
    So we follow the current trend set by Android with
    "show system" and make it "show overlays" on default.
    
    Change-Id: I1cd4d4d3acd767a87fe402c325a872a4c04e0438
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- src/com/android/settings/applications/ManageApplications.java
+++ src/com/android/settings/applications/ManageApplications.java
@@ -121,6 +121,7 @@ public class ManageApplications extends InstrumentedPreferenceFragment
 
     private static final String EXTRA_SORT_ORDER = "sortOrder";
     private static final String EXTRA_SHOW_SYSTEM = "showSystem";
+    private static final String EXTRA_SHOW_SUBSTRATUM = "showSubstratum";
     private static final String EXTRA_HAS_ENTRIES = "hasEntries";
     private static final String EXTRA_HAS_BRIDGE = "hasBridge";
 
@@ -151,6 +152,7 @@ public class ManageApplications extends InstrumentedPreferenceFragment
     public static final int FILTER_APPS_WRITE_SETTINGS = 11;
     public static final int FILTER_APPS_INSTALL_SOURCES = 12;
     public static final int FILTER_APPS_COUNT = 13;  // This should always be the last entry
+    public static final int FILTER_APPS_SUBSTRATUM = 16;
 
     // Mapping to string labels for the FILTER_APPS_* constants above.
     @IdRes
@@ -230,6 +232,9 @@ public class ManageApplications extends InstrumentedPreferenceFragment
 
     // whether showing system apps.
     private boolean mShowSystem;
+
+    // whether showing substratum overlays.
+    private boolean mShowSubstratum;
 
     private ApplicationsState mApplicationsState;
 
@@ -347,6 +352,7 @@ public class ManageApplications extends InstrumentedPreferenceFragment
         if (savedInstanceState != null) {
             mSortOrder = savedInstanceState.getInt(EXTRA_SORT_ORDER, mSortOrder);
             mShowSystem = savedInstanceState.getBoolean(EXTRA_SHOW_SYSTEM, mShowSystem);
+            mShowSubstratum = savedInstanceState.getBoolean(EXTRA_SHOW_SUBSTRATUM, mShowSubstratum);
         }
 
         mInvalidSizeStr = getActivity().getText(R.string.invalid_size_value);
@@ -559,6 +565,7 @@ public class ManageApplications extends InstrumentedPreferenceFragment
         mResetAppsHelper.onSaveInstanceState(outState);
         outState.putInt(EXTRA_SORT_ORDER, mSortOrder);
         outState.putBoolean(EXTRA_SHOW_SYSTEM, mShowSystem);
+        outState.putBoolean(EXTRA_SHOW_SUBSTRATUM, mShowSubstratum);
         outState.putBoolean(EXTRA_HAS_ENTRIES, mApplications.mHasReceivedLoadEntries);
         outState.putBoolean(EXTRA_HAS_BRIDGE, mApplications.mHasReceivedBridgeCallback);
     }
@@ -696,6 +703,11 @@ public class ManageApplications extends InstrumentedPreferenceFragment
                 && mListType != LIST_TYPE_HIGH_POWER);
 
         mOptionsMenu.findItem(R.id.reset_app_preferences).setVisible(mListType == LIST_TYPE_MAIN);
+
+        mOptionsMenu.findItem(R.id.show_substratum).setVisible(!mShowSubstratum
+                && mListType != LIST_TYPE_HIGH_POWER);
+        mOptionsMenu.findItem(R.id.hide_substratum).setVisible(mShowSubstratum
+                && mListType != LIST_TYPE_HIGH_POWER);
     }
 
     @Override
@@ -713,6 +725,11 @@ public class ManageApplications extends InstrumentedPreferenceFragment
             case R.id.show_system:
             case R.id.hide_system:
                 mShowSystem = !mShowSystem;
+                mApplications.rebuild(false);
+                break;
+            case R.id.show_substratum:
+            case R.id.hide_substratum:
+                mShowSubstratum = !mShowSubstratum;
                 mApplications.rebuild(false);
                 break;
             case R.id.reset_app_preferences:
@@ -1050,13 +1067,18 @@ public class ManageApplications extends InstrumentedPreferenceFragment
             if (mCompositeFilter != null) {
                 filterObj = new CompoundFilter(filterObj, mCompositeFilter);
             }
-            if (!mManageApplications.mShowSystem) {
+            if (!mManageApplications.mShowSystem && !mManageApplications.mShowSubstratum) {
                 if (LIST_TYPES_WITH_INSTANT.contains(mManageApplications.mListType)) {
                     filterObj = new CompoundFilter(filterObj,
                             ApplicationsState.FILTER_DOWNLOADED_AND_LAUNCHER_AND_INSTANT);
-                } else {
                     filterObj = new CompoundFilter(filterObj,
-                            ApplicationsState.FILTER_DOWNLOADED_AND_LAUNCHER);
+                            ApplicationsState.FILTER_SUBSTRATUM);
+                } else if (!mManageApplications.mShowSystem) {
+                    filterObj = new CompoundFilter(filterObj,
+                            ApplicationsState.FILTER_DOWNLOADED_AND_LAUNCHER_AND_INSTANT);
+                } else if (!mManageApplications.mShowSubstratum) {
+                    filterObj = new CompoundFilter(filterObj,
+                            ApplicationsState.FILTER_SUBSTRATUM);
                 }
             }
             switch (mLastSortMode) {
