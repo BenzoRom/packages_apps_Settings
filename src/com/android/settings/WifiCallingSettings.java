7cf6048b08ea006d0f99c525dbcd2215a577a362
Kazuya Ohshiro <kazuya.x.ohshiro@sonymobile.com>
2016-08-29 18:29:35 +0900

    Add description for Wifi calling mode
    Replace the view of dialog with custom view that adds description
    as summary.
    
    Test: manual
    Bug: 34793636
    
    Change-Id: Ia5262df95c3a2544e40d28b560b324cc017690a7
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- src/com/android/settings/WifiCallingSettings.java
+++ src/com/android/settings/WifiCallingSettings.java
@@ -77,8 +77,8 @@ public class WifiCallingSettings extends SettingsPreferenceFragment
     //UI objects
     private SwitchBar mSwitchBar;
     private Switch mSwitch;
-    private ListPreference mButtonWfcMode;
-    private ListPreference mButtonWfcRoamingMode;
+    private ListWithEntrySummaryPreference mButtonWfcMode;
+    private ListWithEntrySummaryPreference mButtonWfcRoamingMode;
     private Preference mUpdateAddress;
     private TextView mEmptyView;
 
@@ -221,10 +221,10 @@ public class WifiCallingSettings extends SettingsPreferenceFragment
 
         addPreferencesFromResource(R.xml.wifi_calling_settings);
 
-        mButtonWfcMode = (ListPreference) findPreference(BUTTON_WFC_MODE);
+        mButtonWfcMode = (ListWithEntrySummaryPreference) findPreference(BUTTON_WFC_MODE);
         mButtonWfcMode.setOnPreferenceChangeListener(this);
 
-        mButtonWfcRoamingMode = (ListPreference) findPreference(BUTTON_WFC_ROAMING_MODE);
+        mButtonWfcRoamingMode = (ListWithEntrySummaryPreference) findPreference(BUTTON_WFC_ROAMING_MODE);
         mButtonWfcRoamingMode.setOnPreferenceChangeListener(this);
 
         mUpdateAddress = (Preference) findPreference(PREFERENCE_EMERGENCY_ADDRESS);
@@ -250,10 +250,13 @@ public class WifiCallingSettings extends SettingsPreferenceFragment
         if (!isWifiOnlySupported) {
             mButtonWfcMode.setEntries(R.array.wifi_calling_mode_choices_without_wifi_only);
             mButtonWfcMode.setEntryValues(R.array.wifi_calling_mode_values_without_wifi_only);
+            mButtonWfcMode.setEntrySummaries(R.array.wifi_calling_mode_summaries_without_wifi_only);
+
             mButtonWfcRoamingMode.setEntries(
                     R.array.wifi_calling_mode_choices_v2_without_wifi_only);
             mButtonWfcRoamingMode.setEntryValues(
                     R.array.wifi_calling_mode_values_without_wifi_only);
+            mButtonWfcRoamingMode.setEntrySummaries(R.array.wifi_calling_mode_summaries_without_wifi_only);
         }
     }
 
