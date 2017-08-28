c91dfbb966965f6b79638dcf3a992a4f7e5b575d
maxwen <max.weninger@gmail.com>
2017-08-28 17:15:28 +0200

    Global Actions with Advanced Reboot [3/3]
    mydongistiny: Change setting for lockdown
    Change-Id: I601b4fb4a25e528767bcad7861446c2e8cdd377d
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -149,6 +149,7 @@ import com.android.settings.wifi.savedaccesspoints.SavedAccessPointsWifiSettings
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
 import com.benzo.settings.BenzoSettings;
+import com.benzo.settings.GlobalActionsSettings;
 
 public class SettingsGateway {
 
@@ -286,7 +287,8 @@ public class SettingsGateway {
             MobileNetworkListFragment.class.getName(),
             GlobalActionsPanelSettings.class.getName(),
             DarkModeSettingsFragment.class.getName(),
-            BenzoSettings.class.getName()
+            BenzoSettings.class.getName(),
+            GlobalActionsSettings.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -326,5 +328,6 @@ public class SettingsGateway {
             Settings.ModuleLicensesActivity.class.getName(),
             UserBackupSettingsActivity.class.getName(),
             Settings.BenzoSettingsActivity.class.getName(),
+            Settings.GlobalActionsSettingsActivity.class.getName(),
     };
 }
