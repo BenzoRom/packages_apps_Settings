18a64dbda63caa99059b48a878781178733f09f9
Joe Maples <joe@frap129.org>
2017-09-08 14:05:31 -0500

    Settings: Integrate BenzoExtras into Settings
    Based on both DU's Nougat and Omni's Oreo implimentations, so
    credits to mazda and maxwen for this.
    
    Change-Id: Id4e5367bab5826bfc8bced5684fc2e599f8140c4
    Signed-off-by: Joe Maples <joe@frap129.org>
    [jaysonedson: Ported to android11/12 and Android.bp]
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -325,4 +325,9 @@ public class Settings extends SettingsActivity {
      * Activity for AppDashboard.
      */
     public static class AppDashboardActivity extends SettingsActivity {}
+
+    /**
+     * Activity for Benzo Extras
+     */
+    public static class BenzoSettingsActivity extends SettingsActivity {}
 }
