28499003e4e25d53d211cd11702068393d344937
Sam Mortimer <sam@mortimer.me.uk>
2017-10-27 00:38:00 -0700

    Settings: Battery and Notification Lights [2/2]
    @mydongistiny:
    -Moved from Lineage-SDK
    -Fixed for Settings
    -Fixed LED preview
    
    Change-Id: I069b0163f2a1b550e6c099770e137dd76b546887
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- src/com/android/settings/SettingsPreferenceFragment.java
+++ src/com/android/settings/SettingsPreferenceFragment.java
@@ -50,6 +50,7 @@ import com.android.settings.core.InstrumentedPreferenceFragment;
 import com.android.settings.core.instrumentation.Instrumentable;
 import com.android.settings.core.instrumentation.InstrumentedDialogFragment;
 import com.android.settings.widget.LoadingViewController;
+import com.android.settings.widget.CustomDialogPreferenceLOS;
 import com.android.settingslib.CustomDialogPreference;
 import com.android.settingslib.CustomEditTextPreference;
 import com.android.settingslib.HelpUtils;
@@ -589,7 +590,10 @@ public abstract class SettingsPreferenceFragment extends InstrumentedPreferenceF
             preference.setKey(UUID.randomUUID().toString());
         }
         DialogFragment f = null;
-        if (preference instanceof RestrictedListPreference) {
+        if (preference instanceof CustomDialogPreference) {
+            f = CustomDialogPreference.CustomPreferenceDialogFragment
+                    .newInstance(preference.getKey());
+        } else if (preference instanceof RestrictedListPreference) {
             f = RestrictedListPreference.RestrictedListPreferenceDialogFragment
                     .newInstance(preference.getKey());
         } else if (preference instanceof CustomListPreference) {
@@ -600,6 +604,9 @@ public abstract class SettingsPreferenceFragment extends InstrumentedPreferenceF
                     .newInstance(preference.getKey());
         } else if (preference instanceof CustomEditTextPreference) {
             f = CustomEditTextPreference.CustomPreferenceDialogFragment
+                    .newInstance(preference.getKey());
+        } else if (preference instanceof CustomDialogPreferenceLOS) {
+            f = CustomDialogPreferenceLOS.CustomPreferenceDialogFragment
                     .newInstance(preference.getKey());
         } else {
             super.onDisplayPreferenceDialog(preference);
