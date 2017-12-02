34c0ed5f64b525ea287629756723bf3dddfe07b4
Bruno Martins <bgcngm@gmail.com>
2017-12-02 12:52:18 +0000

    Settings: Add proximity check on wake preference
    jaysonedson: Converted to PreferenceController
    Change-Id: I7516c833ff39de6bdb36c815a5292606cbaa0533
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>

---
+++ src/com/android/settings/display/ProximityOnWakePreferenceController.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2021 Benzo Rom
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.settings.display;
+
+import android.content.Context;
+import android.provider.Settings;
+
+import com.android.settings.R;
+import com.android.settings.core.TogglePreferenceController;
+
+public class ProximityOnWakePreferenceController extends TogglePreferenceController {
+
+    private static final String PROXIMITY_ON_WAKE = "proximity_on_wake";
+    private static final int OFF = 0;
+    private static final int ON = 1;
+
+    public ProximityOnWakePreferenceController(Context context, String key) {
+        super(context, key);
+    }
+
+    @Override
+    public int getSliceHighlightMenuRes() {
+        return R.string.menu_key_display;
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_proximityCheckOnWake)
+                        ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
+    }
+
+    @Override
+    public boolean isChecked() {
+        return Settings.Secure.getInt(mContext.getContentResolver(),
+                Settings.Secure.PROXIMITY_ON_WAKE,
+                OFF) == ON;
+    }
+
+    @Override
+    public boolean setChecked(boolean isChecked) {
+        Settings.Secure.putInt(mContext.getContentResolver(),
+                Settings.Secure.PROXIMITY_ON_WAKE,
+                isChecked ? ON : OFF);
+        return true;
+    }
+}
