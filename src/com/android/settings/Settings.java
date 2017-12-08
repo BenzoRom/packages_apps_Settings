CarbonROM/android_packages_apps_Settings

Settings: Integrate BenzoExtras into Settings
Based on both DU's Nougat and Omni's Oreo implimentations, so
credits to mazda and maxwen for this.

@mydongistiny: fixed up some for dashboard

Change-Id: Id4e5367bab5826bfc8bced5684fc2e599f8140c4
Signed-off-by: Joe Maples <joe@frap129.org>
Signed-off-by: mydongistiny <jaysonedson@gmail.com>

src/com/android/settings/Settings.java
@@ -178,4 +178,5 @@ public class Settings extends SettingsActivity {
     public static class SystemDashboardActivity extends SettingsActivity {}
     public static class SupportDashboardActivity extends SettingsActivity {}
 
+    public static class CarbonFibersActivity extends SettingsActivity { /* empty */ }
 }

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -127,6 +127,8 @@ import com.android.settings.wifi.WifiInfo;
 import com.android.settings.wifi.WifiSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 
+import org.carbonrom.carbonfibers.CarbonFibers;
+
 public class SettingsGateway {
 
     /**
@@ -244,6 +246,7 @@ public class SettingsGateway {
             EnterprisePrivacySettings.class.getName(),
             WebViewAppPicker.class.getName(),
             LockscreenDashboardFragment.class.getName(),
+            CarbonFibers.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -281,5 +284,6 @@ public class SettingsGateway {
             Settings.DateTimeSettingsActivity.class.getName(),
             Settings.DeviceInfoSettingsActivity.class.getName(),
             Settings.EnterprisePrivacySettingsActivity.class.getName(),
+            Settings.CarbonFibersActivity.class.getName(),
     };
 }

BenzoRom/packages_apps_Settings

src/com/android/settings/Settings.java
@@ -191,4 +191,5 @@ public class Settings extends SettingsActivity {
     public static class UserAndAccountDashboardActivity extends SettingsActivity {}
     public static class SystemDashboardActivity extends SettingsActivity {}
 
+    public static class BenzoSettingsActivity extends SettingsActivity { /* empty */ }
 }

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -132,6 +132,8 @@ import com.android.settings.wifi.WifiInfo;
 import com.android.settings.wifi.WifiSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 
+import com.benzo.settings.BenzoSettings;
+
 public class SettingsGateway {
 
     /**
@@ -252,7 +254,8 @@ public class SettingsGateway {
             WebViewAppPicker.class.getName(),
             LockscreenDashboardFragment.class.getName(),
             BluetoothDeviceDetailsFragment.class.getName(),
-            DataUsageList.class.getName()
+            DataUsageList.class.getName(),
+            BenzoSettings.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -290,5 +293,6 @@ public class SettingsGateway {
             Settings.DateTimeSettingsActivity.class.getName(),
             Settings.DeviceInfoSettingsActivity.class.getName(),
             Settings.EnterprisePrivacySettingsActivity.class.getName(),
+            Settings.BenzoSettingsActivity.class.getName(),
     };
 }