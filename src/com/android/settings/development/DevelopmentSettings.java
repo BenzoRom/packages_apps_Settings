71e3258b3f15f6ac07c63c7131d60adfc49e7651
mydongistiny <jaysonedson@gmail.com>
2017-12-18 20:28:08 -0800

    Settings: Remove animation scale options from dangerous options
    No need to go back to the AOSP default animation speeds if you disable developer settings
    
    Change-Id: I0ac50bc2e67e904733d22c332028c44897cc4a0f
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- src/com/android/settings/development/DevelopmentSettings.java
+++ src/com/android/settings/development/DevelopmentSettings.java
@@ -901,9 +901,6 @@ public class DevelopmentSettings extends RestrictedSettingsFragment
         writeLogpersistOption(null, true);
         resetMSOBOptions();
         writeLogdSizeOption(null);
-        writeAnimationScaleOption(0, mWindowAnimationScale, null);
-        writeAnimationScaleOption(1, mTransitionAnimationScale, null);
-        writeAnimationScaleOption(2, mAnimatorDurationScale, null);
         // Only poke the color space setting if we control it.
         if (usingDevelopmentColorSpace()) {
             writeSimulateColorSpace(-1);
