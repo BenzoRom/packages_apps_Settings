68bb80ff02c46731de548217d50a813ec3f80916
Fan Zhang <zhfan@google.com>
2018-01-23 13:48:27 -0800

    Actually fix crash when getting unavailable service
    Fixes: 68013053
    Test: robotests
    Change-Id: I6abb16e1c84b82021ce2dd54097fe68857e69129
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- src/com/android/settings/wfd/WifiDisplaySettings.java
+++ src/com/android/settings/wfd/WifiDisplaySettings.java
@@ -22,6 +22,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.database.ContentObserver;
 import android.hardware.display.DisplayManager;
 import android.hardware.display.WifiDisplay;
@@ -213,7 +214,7 @@ public final class WifiDisplaySettings extends SettingsPreferenceFragment {
 
     public static boolean isAvailable(Context context) {
         return context.getSystemService(Context.DISPLAY_SERVICE) != null
-                && context.getSystemService(Context.WIFI_P2P_SERVICE) != null;
+                && context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_WIFI_DIRECT);
     }
 
     private void scheduleUpdate(int changes) {
