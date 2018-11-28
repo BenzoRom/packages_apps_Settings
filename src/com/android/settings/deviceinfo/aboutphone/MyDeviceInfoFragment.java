4a53f8bdb82b0e12486663db26eb0207ad5fac4c
mydongistiny <jaysonedson@gmail.com>
2018-11-28 03:55:49 -0800

    DeviceInfo: Add back SELinux status to About Phone
    Change-Id: I3bfabe4fbf8e30d944474b772f57d2e2b1c867d7
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- src/com/android/settings/deviceinfo/aboutphone/MyDeviceInfoFragment.java
+++ src/com/android/settings/deviceinfo/aboutphone/MyDeviceInfoFragment.java
@@ -39,6 +39,7 @@ import com.android.settings.deviceinfo.IpAddressPreferenceController;
 import com.android.settings.deviceinfo.ManualPreferenceController;
 import com.android.settings.deviceinfo.RegulatoryInfoPreferenceController;
 import com.android.settings.deviceinfo.SafetyInfoPreferenceController;
+import com.android.settings.deviceinfo.SELinuxStatusPreferenceController;
 import com.android.settings.deviceinfo.UptimePreferenceController;
 import com.android.settings.deviceinfo.WifiMacAddressPreferenceController;
 import com.android.settings.deviceinfo.imei.ImeiInfoPreferenceController;
@@ -117,6 +118,7 @@ public class MyDeviceInfoFragment extends DashboardFragment
         controllers.add(new FccEquipmentIdPreferenceController(context));
         controllers.add(new UptimePreferenceController(context, lifecycle));
         controllers.add(new BenzoVersionPreferenceController(context));
+        controllers.add(new SELinuxStatusPreferenceController(context));
         return controllers;
     }
 
